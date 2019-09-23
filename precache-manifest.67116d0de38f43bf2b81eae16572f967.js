b5f46cee1eda89070a9727ecf3ffd216e84563e8
HCPioquinto <harveycharles.pioquinto@devpartners.co>
2019-09-23 22:16:13 +0800

    Updates

--- precache-manifest.67116d0de38f43bf2b81eae16572f967.js
+++ precache-manifest.67116d0de38f43bf2b81eae16572f967.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "338e2d004b20d343655f3666466c1140",
+    "revision": "491636e8294305a16aa22bf9d969cdba",
     "url": "/school-form/index.html"
   },
   {
-    "revision": "6bfd4b1d2f9674070e57",
-    "url": "/school-form/static/css/main.aa419e78.chunk.css"
+    "revision": "a66b657b5979b924742b",
+    "url": "/school-form/static/css/main.647e7801.chunk.css"
   },
   {
     "revision": "0f34d4540d6ab7d90f7d",
     "url": "/school-form/static/js/2.f53196a2.chunk.js"
   },
   {
-    "revision": "6bfd4b1d2f9674070e57",
-    "url": "/school-form/static/js/main.c799115f.chunk.js"
+    "revision": "a66b657b5979b924742b",
+    "url": "/school-form/static/js/main.b45ab789.chunk.js"
   },
   {
     "revision": "a461f62bf5919a6181d7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/school-form/precache-manifest.4d1ec9cc0c8de428e4faecf27dcc9ce5.js"
+  "/school-form/precache-manifest.67116d0de38f43bf2b81eae16572f967.js"
 );
 
 self.addEventListener('message', (event) => {
