c4e6cc50f09ab04751ad131fa22887f68a4bb779
HCPioquinto <harveycharles.pioquinto@devpartners.co>
2019-09-23 08:32:11 +0800

    Updates

---
+++ precache-manifest.34b271a58e6847e2dfb8e03a23f5e9b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f76644251f1f17dff26ec86e4d9865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "033ab9747beb120acf35",
+    "url": "/static/css/main.cb31f580.chunk.css"
+  },
+  {
+    "revision": "0f34d4540d6ab7d90f7d",
+    "url": "/static/js/2.f53196a2.chunk.js"
+  },
+  {
+    "revision": "033ab9747beb120acf35",
+    "url": "/static/js/main.6f0f8486.chunk.js"
+  },
+  {
+    "revision": "56f7a60439c65a7d1c30",
+    "url": "/static/js/runtime~main.7bd77cdd.js"
+  }
+]);
